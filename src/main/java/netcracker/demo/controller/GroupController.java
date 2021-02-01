3202ccfea5099a6c8f8bc1a1a2a73e0678c2122f
Никита Ярый <nikitacpmi@gmail.com>
2021-02-01 23:44:31 +0400

    Добавлен контролер для поиска всех студентов у группы

--- src/main/java/netcracker/demo/controller/GroupController.java
+++ src/main/java/netcracker/demo/controller/GroupController.java
@@ -1,7 +1,9 @@
 package netcracker.demo.controller;
 
 import netcracker.demo.model.Group;
+import netcracker.demo.model.Student;
 import netcracker.demo.service.GroupService;
+import netcracker.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,47 +15,57 @@ import java.util.List;
 @RequestMapping("/groups/")
 public class GroupController {
     private final GroupService groupService;
+    private final StudentService studentService;
 
     @Autowired
-    public GroupController(GroupService groupService) {
+    public GroupController(GroupService groupService, StudentService studentService) {
         this.groupService = groupService;
+        this.studentService = studentService;
     }
 
     @GetMapping("/")
-    public String showGroups(Model model){
+    public String showGroups(Model model) {
         List<Group> groups = groupService.findAll();
         model.addAttribute("groups", groups);
         return "group/list-groups";
     }
 
     @GetMapping("/create")
-    public String createGroupForm(Group group){
+    public String createGroupForm(Group group) {
         return "group/create-group";
     }
 
     @PostMapping("/create")
-    public String createGroup(Group group){
+    public String createGroup(Group group) {
         groupService.save(group);
         return "redirect:/groups/";
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteGroup(@PathVariable("id") Integer id){
+    public String deleteGroup(@PathVariable("id") Integer id) {
         Group group = groupService.findById(id);
         groupService.delete(group);
         return "redirect:/groups/";
     }
 
     @GetMapping("/update/{id}")
-    public String updateGroupForm(@PathVariable("id") Integer id, Model model){
+    public String updateGroupForm(@PathVariable("id") Integer id, Model model) {
         Group group = groupService.findById(id);
         model.addAttribute("group", group);
         return "group/update-group";
     }
 
     @PostMapping("/update")
-    public String updateGroup(Group group){
+    public String updateGroup(Group group) {
         groupService.save(group);
         return "redirect:/groups/";
+    }
+
+    @GetMapping("/search/{id}")
+    public String searchStudentsByGroup(@PathVariable("id") Integer id, Model model) {
+        String number = groupService.findById(id).getNumber();
+        List<Student> students = studentService.findStudentsByGroupNumber(number);
+        model.addAttribute("students", students);
+        return "group/list-students";
     }
 }
